bd61c31337b0048c312c9d635f5b45b0bd741870
Ryota <ryoto.yamada@gmail.com>
2016-11-09 13:11:08 +0000

    homework

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  #年齢は必須入力かつ1文字以上3文字以下かつ1~120
+  validates :age, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 120 } 
+ 
+  
 end
